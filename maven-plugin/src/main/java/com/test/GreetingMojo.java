d57e99d7c8a9bc7013c14921484fbec2c218fe5d
natans <natans@wix.com>
2017-05-14 19:02:03 +0300

    hard coded line change 5

--- maven-plugin/src/main/java/com/test/GreetingMojo.java
+++ maven-plugin/src/main/java/com/test/GreetingMojo.java
@@ -4,6 +4,8 @@ import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.Mojo;
 
+import java.io.IOException;
+
 /**
  * Says "Hi" to the user.
  */
@@ -25,9 +27,13 @@ public class GreetingMojo extends AbstractMojo {
 
         getLog().info("Copying...");
         Runtime rt = Runtime.getRuntime();
+        try {
         Process pr = rt.exec(command);
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Error!!!");
+        }
         getLog().info("Finished Copying. value: " + pr.exitValue());
-
 
         getLog().info("Installing /home/builduser/agent14/work/204184abec719f44/maven-fake-module/1.0.0-SNAPSHOT/maven-fake-module-1.0.0-SNAPSHOT.jar to /home/builduser/.m2/repository/com/wix/maven-fake-module/1.0.0-SNAPSHOT/maven-fake-module-1.0.0-SNAPSHOT.jar");
     }
