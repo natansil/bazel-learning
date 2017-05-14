a3436cfd5e090933ada14bf68ff135d89acdc287
natans <natans@wix.com>
2017-05-14 18:33:20 +0300

    hard coded line change

--- maven-plugin/src/main/java/com/test/GreetingMojo.java
+++ maven-plugin/src/main/java/com/test/GreetingMojo.java
@@ -13,6 +13,6 @@ public class GreetingMojo extends AbstractMojo
 {
     public void execute() throws MojoExecutionException
     {
-        getLog().info( "Installing /home/builduser/agent14/work/204184abec719f44/payment-framework/target/payment-framework-1.90.0-SNAPSHOT.jar to /home/builduser/.m2/repository/com/wixpress/payment/payment-framework/1.90.0-SNAPSHOT/payment-framework-1.90.0-SNAPSHOT.jar" );
+        getLog().info( "Installing /home/builduser/agent14/work/204184abec719f44/maven-fake-module/1.0.0-SNAPSHOT/maven-fake-module-1.0.0-SNAPSHOT.jar to /home/builduser/.m2/repository/com/wix/maven-fake-module/1.0.0-SNAPSHOT/maven-fake-module-1.0.0-SNAPSHOT.jar" );
     }
 }
