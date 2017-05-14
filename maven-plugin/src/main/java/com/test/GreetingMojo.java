3e5652aa45ab58ceb9a1000c90232b7b472e1ee6
natans <natans@wix.com>
2017-05-14 18:46:24 +0300

    hard coded line change 4

--- maven-plugin/src/main/java/com/test/GreetingMojo.java
+++ maven-plugin/src/main/java/com/test/GreetingMojo.java
@@ -6,14 +6,11 @@ import org.apache.maven.plugins.annotations.Mojo;
 
 /**
  * Says "Hi" to the user.
- *
  */
-@Mojo( name = "sayhi")
-public class GreetingMojo extends AbstractMojo
-{
-    public void execute() throws MojoExecutionException
-    {
-        getLog().info("hello!");
-        getLog().info( "Installing /home/builduser/agent14/work/204184abec719f44/maven-fake-module/1.0.0-SNAPSHOT/maven-fake-module-1.0.0-SNAPSHOT.jar to /home/builduser/.m2/repository/com/wix/maven-fake-module/1.0.0-SNAPSHOT/maven-fake-module-1.0.0-SNAPSHOT.jar" );
+@Mojo(name = "sayhi")
+public class GreetingMojo extends AbstractMojo {
+    public void execute() throws MojoExecutionException {
+        getLog().info("Working Directory = " + System.getProperty("user.dir"));
+        getLog().info("Installing /home/builduser/agent14/work/204184abec719f44/maven-fake-module/1.0.0-SNAPSHOT/maven-fake-module-1.0.0-SNAPSHOT.jar to /home/builduser/.m2/repository/com/wix/maven-fake-module/1.0.0-SNAPSHOT/maven-fake-module-1.0.0-SNAPSHOT.jar");
     }
 }
