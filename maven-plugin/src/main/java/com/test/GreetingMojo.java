284548f3398a2aa78311ffe4a29f0ac0b9dc0c05
natans <natans@wix.com>
2017-05-14 18:59:14 +0300

    hard coded line change 5

--- maven-plugin/src/main/java/com/test/GreetingMojo.java
+++ maven-plugin/src/main/java/com/test/GreetingMojo.java
@@ -13,12 +13,21 @@ public class GreetingMojo extends AbstractMojo {
 
 
         String checkoutDir = System.getProperty("user.dir");
-        String repoDir = checkoutDir + "/../../../.m2/repository/com/wix/maven-fake-module/1.0.0-SNAPSHOT";
-        String projDir = checkoutDir + "/maven-fake-module/target";
+        String repoDir = checkoutDir + "/../../../.m2/repository/com/wix/maven-fake-module/1.0.0-SNAPSHOT/fake-maven-module-1.0.1-SNAPSHOT.jar";
+        String projDir = checkoutDir + "/maven-fake-module/target/fake-maven-module-1.0.1-SNAPSHOT.jar";
+        String command = "cp " + projDir +  " " + repoDir;
 
         getLog().info("Working Directory = " + checkoutDir);
         getLog().info("Repo Directory = " + repoDir);
         getLog().info("proj Directory = " + projDir);
+        getLog().info("command = " + command);
+
+
+        getLog().info("Copying...");
+        Runtime rt = Runtime.getRuntime();
+        Process pr = rt.exec(command);
+        getLog().info("Finished Copying. value: " + pr.exitValue());
+
 
         getLog().info("Installing /home/builduser/agent14/work/204184abec719f44/maven-fake-module/1.0.0-SNAPSHOT/maven-fake-module-1.0.0-SNAPSHOT.jar to /home/builduser/.m2/repository/com/wix/maven-fake-module/1.0.0-SNAPSHOT/maven-fake-module-1.0.0-SNAPSHOT.jar");
     }
